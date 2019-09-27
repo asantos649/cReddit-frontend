db43f5de59490ac98ce5871445efb863366590bf
Andrew Santos <asantos@alumni.nd.edu>
2019-09-27 13:17:44 -0400

    frontend fetch

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import { Input, Menu } from 'semantic-ui-react'
+import logo from '../logo.svg';
 
 class NavBar extends React.Component{
-    render(){
-        return(
-            null
-        )
+    state = { activeItem: 'home' }
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  
+    render() {
+      const { activeItem } = this.state
+  
+      return (
+          
+        <Menu secondary>
+          <Menu.Item>
+            <img src={logo} alt='cReddit'></img>
+          </Menu.Item>
+        <Menu.Item>    
+            <Input icon='search' placeholder='Search...' />
+        </Menu.Item>
+        <Menu.Menu position='right'>
+            
+            <Menu.Item
+            name='POST'
+            active={activeItem === 'home'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='TOPICS'
+            active={activeItem === 'messages'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='PROFILE'
+            active={activeItem === 'friends'}
+            onClick={this.handleItemClick}
+          />
+            <Menu.Item
+              name='LOGOUT'
+              active={activeItem === 'logout'}
+              onClick={this.handleItemClick}
+            />
+        </Menu.Menu>
+        </Menu>
+      )
     }
 }
 
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,9 +4,31 @@ import NavBar from '../components/NavBar'
 import ContentContainer from './ContentContainer'
 
 class MainContainer extends React.Component{
+
+    
+
+    state = {
+        postCollection: [],
+        topicsList: []
+
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3000/posts')
+        .then(resp => resp.json())
+        .then(data => {
+            this.setState ({
+                postCollection: data
+            })
+            console.log(data)
+        })
+    }
     render(){
         return(
-            null
+            <div>
+                <NavBar />
+                <Topics />
+            </div>
         )
     }
 }
